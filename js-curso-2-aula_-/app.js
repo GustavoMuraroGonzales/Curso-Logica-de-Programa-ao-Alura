02ec9342a3bf9227c5b0627082913ec1c1103c09
GustavoGonzales <gustavo.gonzales.tec@gmail.com>
2024-01-22 14:40:16 -0300

    update 1.1

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,5 +1,31 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = "Jogo Do Número Secreto";
+let numeroSecreto = gerarNumeroAleatorio();
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = "Escolha um número entre 1 e 10"
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+exibirTextoNaTela('P', 'Escolha um número entre 1 e 100');
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Acertou!!');
+        exibirTextoNaTela('p', 'Você descobriu o número secreto!');
+    }else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        }else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+            
+    }
+
+}
+
+function gerarNumeroAleatorio () {
+    return parseInt(Math.random() * 100 + 1);
+
+}
