82b5617567251d6800a064a07de22825fa2885c1
GustavoGonzales <gustavo.gonzales.tec@gmail.com>
2024-01-22 15:48:28 -0300

    update 1.5
    adicionado responsive voice

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,9 +1,13 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
+
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
 function exibirMensagemInicial() {
@@ -35,7 +39,18 @@ function verificarChute() {
 }
 
 function gerarNumeroAleatorio () {
-    return parseInt(Math.random() * 100 + 1);
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if(quantidadeDeElementosNaLista == numeroLimite){
+        listaDeNumerosSorteados = [];
+    }
+    if(listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    }else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
 
 }
 
